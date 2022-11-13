2eac5dad33691aaa50afe82c8abf757ecb774c2a
RanJets <rmdejesus27@gmail.com>
2022-11-13 19:19:45 +0800

    Initializing Lesson-2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <User name="Rhandell" age={21} email="del@del.com" />
-      <User name="Russel" age={22} email="sel@sel.com" />
-      <User name="Ynah" age={14} email="ynah@ynah.com" />
-    </div>
-  );
+  return <div className="App"></div>;
 }
-
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.email}</h1>
-    </div>
-  );
-};
 
 export default App;
