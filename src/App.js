39e189c3a8d102ececbd9a4824274545cb4bab34
RanJets <rmdejesus27@gmail.com>
2022-11-13 18:51:58 +0800

    JSX, Components, Props Lesson 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <User name="Rhandell" age={21} email="del@del.com" />
+      <User name="Russel" age={22} email="sel@sel.com" />
+      <User name="Ynah" age={14} email="ynah@ynah.com" />
+    </div>
+  );
 }
 
-const User = () => {
-  return <div></div>;
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
+    </div>
+  );
 };
 
 export default App;
