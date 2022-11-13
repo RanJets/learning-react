7d0f3b96db03c799cb341b237a93f7c12bb6aace
RanJets <rmdejesus27@gmail.com>
2022-11-13 19:41:54 +0800

    Lesson-2 part-1 --Ternary Operator
    Learning Ternary Operator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
+//Ternary Operator Lesson-2 part-1
 function App() {
-  return <div className="App"></div>;
+  const age = 19;
+  const isGreen = false;
+
+  return (
+    <div className="App">
+      {age >= 18 ? <h1>OVER AGE</h1> : <h2>Under Age</h2>}
+      <h1 style={{ color: isGreen ? "green" : "red" }}>THIS HAS COLOR.</h1>
+
+      {isGreen && <button> This is Green</button>}
+    </div>
+  );
 }
 
 export default App;
