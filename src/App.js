ae5de0576493b9a9d4d0abe827e2d9db5a601690
RanJets <rmdejesus27@gmail.com>
2022-11-13 22:13:31 +0800

    Lesson-2 part-2 Object and Import Export
    I learned how to create objects, creating components into another file and exporting/importing it to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import "./App.css";
-
-//Ternary Operator Lesson-2 part-1
+/*
+ *Learning how to import from other created file inside src folder
+ *In import { User } from "./User"; the {User} is the name of component
+ *and the "./User" is the name of the file
+ */
+import { User } from "./User";
+//Using Object in array with the method .map to loop all array
 function App() {
-  const age = 19;
-  const isGreen = false;
-
+  const users = [
+    { name: "pedro", age: 20 },
+    { name: "juan", age: 23 },
+    { name: "manuel", age: 26 },
+    { name: "miguel", age: 15 },
+  ];
   return (
     <div className="App">
-      {age >= 18 ? <h1>OVER AGE</h1> : <h2>Under Age</h2>}
-      <h1 style={{ color: isGreen ? "green" : "red" }}>THIS HAS COLOR.</h1>
-
-      {isGreen && <button> This is Green</button>}
+      {users.map((user, key) => {
+        return <User name={user.name} age={user.age} />;
+      })}
     </div>
   );
 }
