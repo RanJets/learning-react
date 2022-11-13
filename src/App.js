a4c910159faa365520ef06264a99b5cceb928b2e
RanJets <rmdejesus27@gmail.com>
2022-11-13 23:18:23 +0800

    Finished Exercise-2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
-
+import { Planet } from "./Planets";
 function App() {
-  return <div className="App"></div>;
+  const planets = [
+    { name: "Mercury", isGasPlanet: false },
+    { name: "Venus", isGasPlanet: false },
+    { name: "Earth", isGasPlanet: false },
+    { name: "Mars", isGasPlanet: false },
+    { name: "Jupiter", isGasPlanet: true },
+    { name: "Saturn", isGasPlanet: true },
+    { name: "Uranus", isGasPlanet: true },
+    { name: "Neptune", isGasPlanet: true },
+  ];
+  return (
+    <div className="App">
+      {planets.map((planet, key) => {
+        //Ternary Operator sample using  ? : if-else
+        return <Planet name={!planet.isGasPlanet ? planet.name : ""} />;
+      })}
+      {planets.map(
+        //Ternary Operator sample using  && as then
+        (planet, key) => planet.isGasPlanet && <h1>{planet.name}</h1>
+      )}
+    </div>
+  );
 }
 
 export default App;
