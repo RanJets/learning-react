303872fcac89bd13108295d063d84a69d5a16c40
RanJets <rmdejesus27@gmail.com>
2022-11-16 17:10:03 +0800

    Initializing Lesson 5

--- src/App.js
+++ src/App.js
@@ -1,61 +1,7 @@
 import "./App.css";
-import { useState } from "react";
-import { Task } from "./Task";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
-
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  };
-
-  const addTask = () => {
-    const task = {
-      id: todoList.length == 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      completed: false,
-    };
-    setTodoList([...todoList, task]);
-  };
-
-  const completeTask = (id) => {
-    setTodoList(
-      todoList.map((task) => {
-        if (task.id === id) {
-          return { ...task, completed: true };
-        } else {
-          return task;
-        }
-      })
-    );
-  };
-  const deleteTask = (id) => {
-    setTodoList(todoList.filter((tasks) => tasks.id != id));
-  };
-
-  return (
-    <div className="App">
-      <div className="addTask">
-        <input onChange={handleChange} />
-        <button onClick={addTask}>Add Task</button>
-      </div>
-
-      <div className="list">
-        {todoList.map((tasks) => {
-          return (
-            <Task
-              taskName={tasks.taskName}
-              id={tasks.id}
-              completed={tasks.completed}
-              deleteTask={deleteTask}
-              completeTask={completeTask}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
