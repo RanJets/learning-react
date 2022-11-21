201e16917166609dcc6d542cedb7f943f8c7532a
RanJets <rmdejesus27@gmail.com>
2022-11-21 12:19:01 +0800

    Lesson-12 Typescript Final
    learning interfaces and enums in ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Person, Country } from "./components/Person";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Person
+        name="Dell"
+        email="pedro@gmail.com"
+        age={21}
+        isMarried={true}
+        friends={["Jessica", "jake", "jerry", "jasmine"]}
+        country={Country.Brazil}
+      />
+    </div>
+  );
 }
 
 export default App;
