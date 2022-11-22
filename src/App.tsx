a56b1beb90acfad43a3f845be8441e6d0fd0e3e1
RanJets <rmdejesus27@gmail.com>
2022-11-22 17:22:12 +0800

    Social Media Project Crud

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/navbar";
+import { CreatePost } from "./pages/create-post/create-post";
 import { Login } from "./pages/login";
 import { Main } from "./pages/main";
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/createpost" element={<CreatePost />} />
         </Routes>
       </Router>
     </div>
