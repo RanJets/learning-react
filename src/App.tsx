885ef558939da65979bfc45bfdd0bce1fd7f903b
RanJets <rmdejesus27@gmail.com>
2022-11-22 11:37:14 +0800

    Social Media Project - initializing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import React from "react";
 import "./App.css";
-import { Person, Country } from "./components/Person";
 
 function App() {
-  return (
-    <div className="App">
-      <Person
-        name="Dell"
-        email="pedro@gmail.com"
-        age={21}
-        isMarried={true}
-        friends={["Jessica", "jake", "jerry", "jasmine"]}
-        country={Country.Brazil}
-      />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
